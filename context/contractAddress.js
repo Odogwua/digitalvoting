f39bf760e3155340b8103251f0fb72d8b97ac0cf
Odogwua <odogwuaaa@gmail.com>
2023-12-01 06:01:02 -1200

    my diary

---
+++ context/contractAddress.js
@@ -0,0 +1,7 @@
+
+  import grandvote from "./Grandvote.json"
+
+  export const contractAddress = "0x5503723BA43498570d1a116F6870ff37905F2329";
+  export const ownerAddress = "0x8e53E820C70D5f62821A5a81a0C0ff2cC84C0456";
+  export const electionABI = grandvote.abi;
+  
